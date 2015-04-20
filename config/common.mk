9c679f9198f965106eaed3555dc1462b56e30544
thanushyon <thanushyon@thanushyon.com>
2015-04-20 09:49:56 +0100

    Change build type

--- config/common.mk
+++ config/common.mk
@@ -256,7 +256,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := TT-UBER
     CM_EXTRAVERSION :=
 endif
 
